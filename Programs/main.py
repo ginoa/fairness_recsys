6e78ea2146d0ad167943ec3f8b84ef31bdbadbf0
ginoa <gino.almondo@gmail.com>
2019-08-03 16:34:03 +0200

    Update main.py

--- Programs/main.py
+++ Programs/main.py
@@ -4,14 +4,17 @@ Created on Sat Aug  3 15:43:02 2019
 
 @authors: Gino Almondo, Elena Beretta
 """
-
 from io import BytesIO
 from zipfile import ZipFile
 from urllib.request import urlopen
+import pandas as pd
 
 
 #%% load data
+
 resp = urlopen("http://files.grouplens.org/datasets/movielens/ml-100k.zip")
 zipfile = ZipFile(BytesIO(resp.read()))
-
+foofile = zipfile.open('ml-100k/u.data')
+user_data = pd.read_csv(foofile, compression='gzip', sep='\\t')
 #%%
+
