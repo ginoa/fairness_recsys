a29216fa4e625a28b7dedc953df8d864a3fc34f1
ginoa <gino.almondo@gmail.com>
2019-08-11 15:54:00 +0200

    Update train_save_model.py

--- Programs/train_save_model.py
+++ Programs/train_save_model.py
@@ -10,35 +10,21 @@ from io import BytesIO
 from zipfile import ZipFile
 from urllib.request import urlopen
 
+os.chdir("C:\\Users\\errat\\Documents\\GitHub\\fairness_recsys\\Programs")
 import model
 
-os.chdir("C:\\Users\\errat\\Documents\\GitHub\\fairness_recsys\\Programs")
-
-#%% set variables
-k = 10
-
-epochs = 10
-display_step = 10
-
-learning_rate = 0.3
-
-batch_size = 250
-
-# load data
+# load training data
 
 resp = urlopen("http://files.grouplens.org/datasets/movielens/ml-100k.zip")
 zipfile = ZipFile(BytesIO(resp.read()))
-#
 foofile = zipfile.open('ml-100k/u1.base')
 df = pd.read_csv(foofile, usecols = [0,1,2], header = None, names = ['user_id','item_id','rating'], sep='\t')
 
 num_items = df.item_id.nunique()
 num_users = df.user_id.nunique()
-
-#%%
-
 print("USERS: {} ITEMS: {}".format(num_users, num_items))
 
+#%% Process data
 
 # Normalize in [0, 1]
 
@@ -69,71 +55,6 @@ print("Matrix shape: {}".format(matrix.shape))
 # print("USERS: {} ITEMS: {}".format(num_users, num_items))
 
 #%%
-# Network Parameters
-
-num_input = num_items   # num of items
-num_hidden_1 = 10       # 1st layer num features
-num_hidden_2 = 5        # 2nd layer num features (the latent dim)
-
-X = tf.placeholder(tf.float64, [None, num_input])
-
-weights = {
-    'encoder_h1': tf.Variable(tf.random_normal([num_input, num_hidden_1], dtype=tf.float64)),
-    'encoder_h2': tf.Variable(tf.random_normal([num_hidden_1, num_hidden_2], dtype=tf.float64)),
-    'decoder_h1': tf.Variable(tf.random_normal([num_hidden_2, num_hidden_1], dtype=tf.float64)),
-    'decoder_h2': tf.Variable(tf.random_normal([num_hidden_1, num_input], dtype=tf.float64)),
-}
-
-biases = {
-    'encoder_b1': tf.Variable(tf.random_normal([num_hidden_1], dtype=tf.float64)),
-    'encoder_b2': tf.Variable(tf.random_normal([num_hidden_2], dtype=tf.float64)),
-    'decoder_b1': tf.Variable(tf.random_normal([num_hidden_1], dtype=tf.float64)),
-    'decoder_b2': tf.Variable(tf.random_normal([num_input], dtype=tf.float64)),
-}
-
-
-# Building the encoder
-
-def encoder(x):
-    # Encoder Hidden layer with sigmoid activation #1
-    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h1']), biases['encoder_b1']))
-    # Encoder Hidden layer with sigmoid activation #2
-    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['encoder_h2']), biases['encoder_b2']))
-    return layer_2
-
-
-# Building the decoder
-
-def decoder(x):
-    # Decoder Hidden layer with sigmoid activation #1
-    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['decoder_h1']), biases['decoder_b1']))
-    # Decoder Hidden layer with sigmoid activation #2
-    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']), biases['decoder_b2']))
-    return layer_2
-
-
-# Construct model
-
-encoder_op = encoder(X)
-decoder_op = decoder(encoder_op)
-
-
-# Prediction
-
-y_pred = decoder_op
-
-
-# Targets are the input data.
-
-y_true = X
-
-
-# Define loss and optimizer, minimize the squared error
-
-loss = tf.losses.mean_squared_error(y_true, y_pred)
-optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(loss)
-
-predictions = pd.DataFrame()
 
 # Define evaluation metrics
 
