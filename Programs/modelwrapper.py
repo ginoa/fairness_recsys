43d06e7ad82219fefd12d38104a672c898bacfcc
ginoa <gino.almondo@gmail.com>
2019-08-11 17:59:32 +0200

    Update modelwrapper.py

--- Programs/modelwrapper.py
+++ Programs/modelwrapper.py
@@ -18,21 +18,11 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from abc import ABCMeta
-from abc import abstractmethod
+import tcav
 import numpy as np
-import six
-import tensorflow as tf
 
-class ModelWrapper(six.with_metaclass(ABCMeta, object)):
-  """Simple wrapper of the for models with session object for TCAV.
-    Supports easy inference with no need to deal with the feed_dicts.
-  """
-
-  @abstractmethod
+class myModelWrapper(tcav.model.ModelWrapper):
+  # custom class for overriding abstract methods of tcav.model.ModelWrapper
   def __init__(self):
     # A dictionary of bottleneck tensors.
     self.bottlenecks_tensors = None
@@ -48,77 +38,10 @@ class ModelWrapper(six.with_metaclass(ABCMeta, object)):
     # The tensor representing the loss (used to calculate derivative).
     self.loss = None
 
-  def _make_gradient_tensors(self):
-    """Makes gradient tensors for all bottleneck tensors.
-    """
-    self.bottlenecks_gradients = {}
-    for bn in self.bottlenecks_tensors:
-      self.bottlenecks_gradients[bn] = tf.gradients(
-          self.loss, self.bottlenecks_tensors[bn])[0]
-
-  def get_gradient(self, acts, y, bottleneck_name, example):
-    """Return the gradient of the loss with respect to the bottleneck_name.
-    Args:
-      acts: activation of the bottleneck
-      y: index of the logit layer
-      bottleneck_name: name of the bottleneck to get gradient wrt.
-      example: input example. Unused by default. Necessary for getting gradients
-        from certain models, such as BERT.
-    Returns:
-      the gradient array.
-    """
-    return self.sess.run(self.bottlenecks_gradients[bottleneck_name], {
-        self.bottlenecks_tensors[bottleneck_name]: acts,
-        self.y_input: y
-    })
-
-  def get_predictions(self, examples):
-    """Get prediction of the examples.
-    Args:
-      imgs: array of examples to get predictions
-    Returns:
-      array of predictions
-    """
-    return self.adjust_prediction(
-        self.sess.run(self.ends['prediction'], {self.ends['input']: examples}))
-
-  def adjust_prediction(self, pred_t):
-    """Adjust the prediction tensor to be the expected shape.
-    Defaults to a no-op, but necessary to override for GoogleNet
-    Returns:
-      pred_t: pred_tensor.
-    """
-    return pred_t
-
-  def reshape_activations(self, layer_acts):
-    """Reshapes layer activations as needed to feed through the model network.
-    Override this for models that require reshaping of the activations for use
-    in TCAV.
-    Args:
-      layer_acts: Activations as returned by run_examples.
-    Returns:
-      Activations in model-dependent form; the default is a squeezed array (i.e.
-      at most one dimensions of size 1).
-    """
-    return np.asarray(layer_acts).squeeze()
-
-  @abstractmethod
   def label_to_id(self, label):
     """Convert label (string) to index in the logit layer (id)."""
     pass
 
-  @abstractmethod
   def id_to_label(self, idx):
     """Convert index in the logit layer (id) to label (string)."""
     pass
-
-  def run_examples(self, examples, bottleneck_name):
-    """Get activations at a bottleneck for provided examples.
-    Args:
-      examples: example data to feed into network.
-      bottleneck_name: string, should be key of self.bottlenecks_tensors
-    Returns:
-      Activations in the given layer.
-    """
-    return self.sess.run(self.bottlenecks_tensors[bottleneck_name],
-                         {self.ends['input']: examples})
